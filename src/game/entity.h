6a8ccd03a9eee11cd890b4d4351044d5e251ebb7
Erik Mattfolk <erik.mattfolk@gmail.com>
2019-11-08 21:12:10 +0100

    Add entity

---
+++ src/game/entity.h
@@ -0,0 +1,7 @@
+struct Entity {
+    Vec2 pos;
+    f32 width;
+    f32 height;
+    f32 rotation;
+    AssetID image;
+};
--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -1,12 +1,19 @@
 // Tell the engine that this is loaded
 #define FOG_GAME
+#define NO_ASSET 1024
+
+#include <vector>
+#include "entity.h"
 
 namespace Game {
 
 using namespace Input;
 
-void setup() {
+std::vector<Entity> entities;
 
+void setup() {
+    entities.push_back({V2(0, 0), 0.3, 0.3, 0.7, ASSET_TEST});
+    entities.push_back({V2(0.5, 0.5), 0.3, 0.3, 0, ASSET_TEST});
 }
 
 // Main logic
@@ -16,7 +23,19 @@ void update(f32 delta) {
 
 // Main draw
 void draw() {
-
+    for (Entity entity : entities) {
+        if (entity.image != NO_ASSET) {
+            Image* img = Asset::fetch_image(entity.image);
+            Renderer::push_sprite(entity.pos,
+                    V2(entity.width, entity.height),
+                    entity.rotation,
+                    entity.image,
+                    V2(0,0),
+                    V2(img->width, img->height));
+        } else {
+            Renderer::push_rectangle(entity.pos, V2(entity.width, entity.height));
+        }
+    }
 }
 
 }  // namespace Game
