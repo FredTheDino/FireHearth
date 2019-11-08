b991a275a5dede76ca9c1971531932e6099b6125
Edvard Thörnros <edvard.thornros@gmail.com>
2019-11-08 21:58:01 +0100

    Fix memory bug.

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -1,11 +1,9 @@
 // Tell the engine that this is loaded
 #define FOG_GAME
 
-#if 0
-
 namespace Game {
-using namespace Input;
 
+using namespace Input;
 Physics::ShapeID square;
 
 #include "truck.h"
@@ -13,8 +11,14 @@ Truck truck;
 
 void setup() {
     add(K(w), Player::P1, Name::UP);
+    add(K(d), Player::P1, Name::UP);
+    add(K(s), Player::P1, Name::DOWN);
+    add(K(a), Player::P1, Name::DOWN);
     add(K(SPACE), Player::P1, Name::BOOST);
     add(K(m), Player::P1, Name::SHOOT);
+
+    Renderer::set_window_size(800, 600);
+    Renderer::set_window_position(200, 100);
 
     {
         Vec2 points[] = {
@@ -23,21 +27,22 @@ void setup() {
             V2( 0.5,  0.5),
             V2(-0.5,  0.5),
         };
-        square = Physics::add_shape(LEN(points), points)
+        square = Physics::add_shape(LEN(points), points);
     }
 
     truck = create_truck();
+
+    Renderer::global_camera.zoom = 1.0 / 20.0;
 }
 
 // Main logic
 void update(f32 delta) {
-    truck.update();
+    truck.update(delta);
 }
 
 // Main draw
 void draw() {
     truck.draw();
 }
-#endif
 
 }  // namespace Game
